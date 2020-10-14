821b8cdb97fbc4e082616152de0e43b60c04a574
Tanya <gasheva_99@mail.ru>
2020-10-14 18:16:50 +0300

    Убрала лишний код для поиска правила с глобальной переменной

--- src/main/java/ru/gasheva/models/MLV.java
+++ src/main/java/ru/gasheva/models/MLV.java
@@ -6,13 +6,21 @@ import ru.gasheva.models.classes.Rule;
 import ru.gasheva.models.classes.Variable;
 
 public class MLV {
-    Variable globalTarget;
-    ConsultationControl control;
-    RuleModel ruleModel;
-    VariableModel variableModel;
-    DomainModel domainModel;
-    WorkingMemory workingMemory;
-    final String UNDEF = "undef";
+    private Variable globalTarget;
+    private ConsultationControl control;
+    private RuleModel ruleModel;
+    private VariableModel variableModel;
+    private DomainModel domainModel;
+    private WorkingMemory workingMemory;
+    private final String UNDEF = "undef";
+
+    public WorkingMemory getWorkingMemory() {
+        return workingMemory;
+    }
+
+    public void setWorkingMemory(WorkingMemory workingMemory) {
+        this.workingMemory = workingMemory;
+    }
 
     public String defineGlobalTarget(Variable globalTarget, RuleModel ruleModel, VariableModel variableModel, DomainModel domainModel, ConsultationControl control) {
         this.ruleModel = ruleModel;
@@ -22,53 +30,54 @@ public class MLV {
         workingMemory = new WorkingMemory();
         this.control = control;
         int i = 0;
-        while (i <= ruleModel.size()) {
-            if (i == ruleModel.size()) {     //если проверили все правила и не нашли подцель
-                workingMemory.add(globalTarget, new DomainValue(UNDEF));
-                return UNDEF;
-            } else {
-                //нашли правило с подцелью
-                Rule curRule = ruleModel.getRule(i);
-                if (!curRule.hasFactInConclusion(globalTarget)) {
-                    i++;
-                    continue;
-                }
-                int j = 0;
-                while (j <= curRule.conditionsSize()) {     // проходим по предпосылкам
-                    if (j == curRule.conditionsSize()) {      //если прошли все, то означиваем подцель
-                        workingMemory.add(curRule);
-                        workingMemory.add(globalTarget, curRule.getConclusion(0).getDomainValue());
-                        return workingMemory.get(globalTarget).getValue();
-                    } else {
-                        Variable curVar = curRule.getCondition(j).getVariable();
-                        if (workingMemory.hasValue(curVar)) {        //если переменная означена, проверяем нужное ли значение
-                            if (workingMemory.get(curVar).equals(curRule.getCondition(j).getDomainValue())) {   //если значение нужное, то переходим к след переменной
-                                j++;
-                                continue;
-                            } else {       //иначе к следующему правилу
-                                i++;
-                                break;
-                            }
-                        } else {  // если переменная не означена
-                            switch (curVar.getVarType()) {
-                                case ASK:
-                                    String val = control.askVariableValue(curVar);
-                                    workingMemory.add(curVar, new DomainValue(val));
-                                case RESOLVE:
-                                    defineLocalTarget(curVar);
-                            }
-                            if (workingMemory.get(curVar).equals(curRule.getCondition(j).getDomainValue())) {   //если значение нужное, то переходим к след переменной
-                                j++;
-                                continue;
-                            } else {       //иначе к следующему правилу
-                                i++;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+//        while (i <= ruleModel.size()) {
+//            if (i == ruleModel.size()) {     //если проверили все правила и не нашли подцель
+//                workingMemory.add(globalTarget, new DomainValue(UNDEF));
+//                return UNDEF;
+//            } else {
+//                //нашли правило с подцелью
+//                Rule curRule = ruleModel.getRule(i);
+//                if (!curRule.hasFactInConclusion(globalTarget)) {
+//                    i++;
+//                    continue;
+//                }
+//                int j = 0;
+//                while (j <= curRule.conditionsSize()) {     // проходим по предпосылкам
+//                    if (j == curRule.conditionsSize()) {      //если прошли все, то означиваем подцель
+//                        workingMemory.add(curRule);
+//                        workingMemory.add(globalTarget, curRule.getConclusion(0).getDomainValue());
+//                        return workingMemory.get(globalTarget).getValue();
+//                    } else {
+//                        Variable curVar = curRule.getCondition(j).getVariable();
+//                        if (workingMemory.hasValue(curVar)) {        //если переменная означена, проверяем нужное ли значение
+//                            if (workingMemory.get(curVar).equals(curRule.getCondition(j).getDomainValue())) {   //если значение нужное, то переходим к след переменной
+//                                j++;
+//                                continue;
+//                            } else {       //иначе к следующему правилу
+//                                i++;
+//                                break;
+//                            }
+//                        } else {  // если переменная не означена
+//                            switch (curVar.getVarType()) {
+//                                case ASK:
+//                                    String val = control.askVariableValue(curVar);
+//                                    workingMemory.add(curVar, new DomainValue(val));
+//                                case RESOLVE:
+//                                    defineLocalTarget(curVar);
+//                            }
+//                            if (workingMemory.get(curVar).equals(curRule.getCondition(j).getDomainValue())) {   //если значение нужное, то переходим к след переменной
+//                                j++;
+//                                continue;
+//                            } else {       //иначе к следующему правилу
+//                                i++;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        defineLocalTarget(globalTarget);
         return workingMemory.get(globalTarget).getValue();
     }
 
